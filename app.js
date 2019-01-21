e91ec4ee8f6364a29d46d1aede30cce0f91db252
Dean Radcliffe <deanmisc@yahoo.com>
2019-01-21 11:52:44 -0600

    WIP

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require("express");
 var path = require("path");
 var process = require("process");
 var rpio = require("rpio");
-var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
@@ -11,6 +10,8 @@ var routes = require("./routes/index");
 var users = require("./routes/users");
 var ajax = require("./routes/ajax");
 
+const { agent, after } = require("rx-helper");
+const { concat } = require("rxjs");
 require("dotenv").config();
 
 var app = express();
@@ -64,11 +65,109 @@ app.use(function(err, req, res, next) {
 
 app.listen(app.get("port"));
 
-var spawn = require("child_process").spawn;
-const { Observable } = require("rxjs");
-const { debounceTime } = require("rxjs/operators");
+// We have a single white (RGB) bulb, we dont control blue, and we control RG on 5/6
+const redPin = 5;
+const greenPin = 6;
+const statusPin = 26;
+setUpAgent();
+agent.process({ type: "start" });
 
+process.on("SIGINT", function() {
+  agent.process({ type: "shutdown" });
+  process.exit();
+});
+process.on("exit", () => agent.process({ type: "shutdown" }));
+
+function setGreen(status = true) {
+  if (status) {
+    // must turn off 5 red to have 6 green on
+    setRPIO({ gpio: redPin, status: false });
+    setRPIO({ gpio: greenPin, status: true });
+  } else {
+    setRPIO({ gpio: greenPin, status: false });
+  }
+}
+
+function setRed(status = true) {
+  if (status) {
+    // turn off 6 green to show 5 red
+    setRPIO({ gpio: greenPin, status: false });
+    setRPIO({ gpio: redPin, status: true });
+  } else {
+    setRPIO({ gpio: redPin, status: false });
+  }
+}
+
+function setStatus(status = true) {
+  setRPIO({ gpio: statusPin, status });
+}
+
+function setRPIO({ gpio, status }) {
+  rpio.open(gpio, rpio.OUTPUT, +status);
+  rpio.write(gpio, +status);
+}
+
+function setUpAgent() {
+  // Filters run synchronously!
+  agent.filter("shutdown", function shutdown() {
+    try {
+      setRed(false);
+      setGreen(false);
+      setStatus(false);
+      [statusPin, greenPin, redPin].forEach(pin => rpio.close(pin));
+    } catch (ex) {
+      console.log("Error: " + ex.message);
+    }
+  });
+
+  agent.on(
+    "start",
+    () => {
+      rpio.init({ mapping: "gpio" });
+      // on startup turn on status
+      setStatus(true);
+      setRed(false);
+      setGreen(false);
+
+      // Return the startup dance
+      return concat(
+        after(2000, () => setStatus(false)),
+        after(500, "red"),
+        after(500, "off"),
+        after(500, "red"),
+        after(500, "off"),
+        after(500, "red"),
+        after(500, "off"),
+        after(500, "red")
+      );
+    },
+    { type: "setColor" }
+  );
+
+  agent.on("setColor", ({ action }) => {
+    const color = action.payload;
+    switch (color) {
+      case "red":
+        setRed(true);
+        break;
+      case "green":
+        setGreen(true);
+        break;
+      case "off":
+        setRed(false);
+        setGreen(false);
+        break;
+    }
+  });
+}
+
+// const { Observable } = require("rxjs");
+// const validKeys = [
+//   "198,169,99,26", // white card
+//   "166,168,89,211" // blue fob
+// ];
 // const rfids = new Observable(notify => {
+// var spawn = require("child_process").spawn;
 //   console.log("spawning python process...");
 //   var child = spawn("sudo", ["python", "/home/pi/src/gpio-gui/rfid/Read.py"]);
 //   child.stdout.setEncoding("utf8");
@@ -91,77 +190,13 @@ const { debounceTime } = require("rxjs/operators");
 //   return () => child.kill();
 // });
 
-const rfid = require("node-rfid");
+// const rfid = require("node-rfid");
 
-const rfids = new Observable(notify => {
-  rfid.read(function(err, result) {
-    if (err) console.log("Sorry, some hardware error occurred"); //some kind of hardware/wire error
-    notify.next(result);
-  });
-});
-
-const validKeys = [
-  "198,169,99,26", // white card
-  "166,168,89,211" // blue fob
-];
-
-// Pin 26 reflects our status
-const statusPin = 26;
-
-// on startup turn on status, off others
-setRPIO({ gpio: statusPin, status: true });
-setRed(false);
-setGreen(false);
-
-// and lock
-setTimeout(setRed, 1500);
-
-let rfidSub;
-rfidSub = rfids.subscribe(
-  rfid => {
-    console.log("Scan: " + rfid);
-    if (validKeys.includes(rfid)) {
-      setGreen();
-    }
-  },
-  e => {
-    console.log("RFID error: " + e);
-  }
-);
-
-process.on("SIGINT", function() {
-  shutdown();
-  process.exit();
-});
-process.on("exit", shutdown);
-
-function shutdown() {
-  try {
-    setRed(false);
-    setGreen(false);
-    setRPIO({ gpio: statusPin, status: false });
-    rpio.close(statusPin);
-    rfidSub && rfidSub.unsubscribe();
-  } catch (ex) {
-    console.log("Error: " + ex.message);
-  }
-}
-
-function setGreen(status = true) {
-  setRPIO({ gpio: 5, status: false });
-  setRPIO({ gpio: 6, status: status && true });
-}
-
-function setRed(status = true) {
-  setRPIO({ gpio: 5, status: status && true });
-  setRPIO({ gpio: 6, status: false });
-}
-
-// We need to mind our init/open/close lifecycle tightly
-function setRPIO({ gpio, status }) {
-  rpio.init({ mapping: "gpio" });
-  rpio.open(gpio, rpio.OUTPUT, +status);
-  rpio.write(gpio, +status);
-}
+// const rfids = new Observable(notify => {
+//   rfid.read(function(err, result) {
+//     if (err) console.log("Sorry, some hardware error occurred"); //some kind of hardware/wire error
+//     notify.next(result);
+//   });
+// });
 
 module.exports = app;
